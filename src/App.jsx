47887957b3fff72303806dcd923c37a39bbf8362
Kidung Fajar Prabowo <kfajarbowo12@gmail.com>
2023-06-02 23:10:47 +0700

    feat : add oauth & middleware for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,107 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { Container } from 'react-bootstrap';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { fetchDataFromApi } from './utils/api';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration, getGenres } from './store/homeSlice';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import RedirectIfProtected from './components/redirectIfProtected/RedirectIfProtected';
+import Protected from './components/protected/Protected';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const dispatch = useDispatch();
+	const { url } = useSelector(state => state.home);
+	console.log(url);
+
+	useEffect(() => {
+		fetchApiConfig();
+		genresCall();
+	}, []);
+
+	const fetchApiConfig = () => {
+		fetchDataFromApi('/configuration').then(res => {
+			console.log(res);
+
+			const url = {
+				backdrop: res.images.secure_base_url + 'original',
+				poster: res.images.secure_base_url + 'original',
+				profile: res.images.secure_base_url + 'original',
+			};
+
+			dispatch(getApiConfiguration(url));
+		});
+	};
+
+	const genresCall = async () => {
+		let promises = [];
+		let endPoints = ['tv', 'movie'];
+		let allGenres = {};
+
+		endPoints.forEach(url => {
+			promises.push(fetchDataFromApi(`/genre/${url}/list`));
+		});
+
+		const data = await Promise.all(promises);
+		console.log(data);
+		data.map(({ genres }) => {
+			return genres.map(item => (allGenres[item.id] = item));
+		});
+
+		dispatch(getGenres(allGenres));
+	};
 
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		<GoogleOAuthProvider clientId={import.meta.env.VITE_APP_GOOGLE_CLIENT_ID}>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route
+						path="/login"
+						element={
+							<RedirectIfProtected>
+								<Login />
+							</RedirectIfProtected>
+						}
+					/>
+					<Route
+						path="/register"
+						element={
+							<RedirectIfProtected>
+								<Register />
+							</RedirectIfProtected>
+						}
+					/>
+					<Route
+						path="/:mediaType/:id"
+						element={
+							<Protected>
+								<Details />
+							</Protected>
+						}
+					/>
+					<Route path="/search/:query" element={<SearchResult />} />
+					<Route path="/explore/:mediaType" element={<Explore />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+				<Footer />
+				<ToastContainer theme="colored" />
+			</BrowserRouter>
+		</GoogleOAuthProvider>
 	);
 }
 

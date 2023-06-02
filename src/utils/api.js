e2da5eba57edec36c6453e1c52477ee3ff22af6a
Kidung Fajar Prabowo <kfajarbowo12@gmail.com>
2023-06-03 00:32:30 +0700

    test

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN =
+	'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlY2I0Zjk1OTI1MDU2MTE3ZmUwMjBhMzIzM2VhMjIyZSIsInN1YiI6IjVmOWFiOTMyZTk0MmVlMDAzN2FkMTIzZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MvaHZj-ihOHQt1KBUlzIHkq_TT70WG-5lG6Uh3vEot0';
 
 const headers = {
 	// Authorization: 'bearer ' + TMDB_TOKEN,
-	"Authorization": `Bearer ${TMDB_TOKEN}`,
+	Authorization: `Bearer ${TMDB_TOKEN}`,
 };
 
 export const fetchDataFromApi = async (url, params) => {
